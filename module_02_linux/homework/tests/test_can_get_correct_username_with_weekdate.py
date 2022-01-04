864d3ccaf88e9f6049e967896650e08debc8dfcc
vlaskinmac <kangelina86@mail.ru>
2022-01-04 14:20:44 +0300

    Added test_correct_weekday()

--- module_02_linux/homework/tests/test_can_get_correct_username_with_weekdate.py
+++ module_02_linux/homework/tests/test_can_get_correct_username_with_weekdate.py
@@ -1,3 +1,4 @@
+import datetime
 import unittest
 
 from module_02_linux.homework.hw_2_4_hello_world_with_day import app
@@ -10,11 +11,27 @@ class TestWeekDate(unittest.TestCase):
         self.app = app.test_client()
         self.base_url = '/hello-world/'
 
-    def test_correct_username_in_weekdate(self):
+    def test_correct_username(self):
         username = 'username'
         response = self.app.get(self.base_url + username)
         response_text = response.data.decode()
         self.assertTrue(username in response_text)
+
+    def test_correct_weekday(self):
+        username = 'username'
+        day = datetime.datetime.today().weekday()
+        week = {
+            0: 'Хорошего Понедельника',
+            1: 'Хорошего Вторника',
+            2: 'Хорошей Среды',
+            3: 'Хорошего Четверга',
+            4: 'Хорошей Пятницы',
+            5: 'Хорошей Субботы',
+            6: 'Хорошего Воскресенья',
+        }
+        response = self.app.get(self.base_url + username)
+        response_text = response.data.decode()
+        self.assertTrue(week[day] in response_text)
 
 
 
