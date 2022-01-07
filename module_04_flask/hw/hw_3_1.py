c1e346ccb0bd5e471eaab44132bc820ebe815312
vlaskinmac <kangelina86@mail.ru>
2022-01-07 14:23:07 +0300

    Done

--- module_04_flask/hw/hw_3_1.py
+++ module_04_flask/hw/hw_3_1.py
@@ -9,19 +9,25 @@
 Endpoint должен быть по url = /ps и принимать входные значение через аргумент arg
 Напомню, вызвать программу ps можно, например, вот так
 
-    >>> import shlex, subprocess
-    >>> command_str = f"ps aux"
-    >>> command = shlex.split(command_str)
-    >>> result = subprocess.run(command, capture_output=True)
+
 """
-from flask import Flask
+
+import shlex, subprocess
+
+from typing import Optional
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route("/ps", methods=["GET"])
+@app.route("/ps/", methods=["GET"])
 def _ps():
-    pass
+    arg: Optional[str] = request.args.get("arg", type=str, default=None)
+    command_str = f"ps {arg}"
+    command = shlex.split(command_str)
+    result = subprocess.run(command, capture_output=True)
+    decode_result = result.stdout.decode('utf-8')
+    return f'<pre> {decode_result} </pre>'
 
 
 if __name__ == "__main__":
