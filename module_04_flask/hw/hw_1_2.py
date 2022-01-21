4faf4ba7fd29caca86fb9746447942aa134e7e4e
vlaskinmac <kangelina86@mail.ru>
2022-01-21 16:47:34 +0300

    Done hw_2_execute_code_from_form

--- module_04_flask/hw/hw_1_2.py
+++ module_04_flask/hw/hw_1_2.py
@@ -38,5 +38,7 @@ def registration():
 
 
 if __name__ == "__main__":
+
+
     app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True)
--- module_06_debugging_begin/hw/hw_2_execute_code_from_form.py
+++ module_06_debugging_begin/hw/hw_2_execute_code_from_form.py
@@ -9,7 +9,7 @@
 4. Добавьте необходимые debug сообщения
 5. Инициализируйте basicConfig для записи логов в stdout с указанием времени
 """
-
+import logging
 import shlex
 import subprocess
 
@@ -17,6 +17,9 @@ from flask import Flask
 from flask_wtf import FlaskForm
 from wtforms import IntegerField, StringField
 from wtforms.validators import InputRequired
+
+
+logger = logging.getLogger("check")
 
 app = Flask(__name__)
 
@@ -27,7 +30,9 @@ class CodeForm(FlaskForm):
 
 
 def run_python_code_in_subprocess(code: str, timeout: int) -> str:
+    logger.debug(f"Input code: {code}")
     command = f'python3 -c "{code}"'
+
     command = shlex.split(command)
     process = subprocess.Popen(
             command,
@@ -36,7 +41,9 @@ def run_python_code_in_subprocess(code: str, timeout: int) -> str:
     )
 
     outs, errs = process.communicate(timeout=timeout)
-
+    if errs:
+        logger.exception("Ошибка stderr", exc_info=errs.decode())
+    logger.debug(f"Stdout is valid: {outs.decode()}")
     return outs.decode()
 
 
@@ -47,12 +54,24 @@ def run_code():
         code = form.code.data
         timeout = form.timeout.data
         stdout = run_python_code_in_subprocess(code=code, timeout=timeout)
+        logger.debug(f"Form is valid {code}, {timeout}")
+        if not stdout:
+            logger.error(f"Stdout is not a code {TypeError.__name__} ")
         return f"Stdout: {stdout}"
-
     return f"Bad request. Error = {form.errors}", 400
 
 
-if __name__ == "__main__":
-    app.config["WTF_CSRF_ENABLED"] = False
+@app.errorhandler(OSError)
+def error_handler(exc):
+    logger.exception("Ошибка", exc_info=exc)
+    return f"Bad request {exc}", 500
 
+
+if __name__ == "__main__":
+    logger.debug("Start server!")
+    logging.basicConfig(level=logging.INFO, filename="exec.log",
+                        format='%(levelname)s --- %(name)s - %(asctime)s %(message)s',
+                        datefmt='%I:%M:%S', filemode="w")
+    app.config["WTF_CSRF_ENABLED"] = False
     app.run(debug=True)
+
