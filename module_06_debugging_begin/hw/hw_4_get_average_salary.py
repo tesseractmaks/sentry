ced88d8f7108075c1596fde1dc995c46551af2b8
vlaskinmac <kangelina86@mail.ru>
2022-01-23 18:25:13 +0300

    Done hw_4

--- module_06_debugging_begin/hw/hw_4_get_average_salary.py
+++ module_06_debugging_begin/hw/hw_4_get_average_salary.py
@@ -15,59 +15,42 @@
 
 Покройте функцию логгированием.
 """
-import random
-from typing import List
+import logging
 
 import numpy as np
 
+from typing import List
+
 
 def get_average_salary_corrected(salaries: List[int]) -> float:
-
-    # x= sorted(list(map(int, array_salaries)))
-    # print(x)
-    # print()
-    #
-    # cut_array_salaries = np.delete(np.sort(array_salaries), [[0], [-1]]),  # np.delete(array_salaries, [-1])
-    #
-    # print(np.count_nonzero(array_salaries))
-    # print(np.count_nonzero(cut_array_salaries))
-    # avg_salaries = np.sum(cut_array_salaries) / np.count_nonzero(cut_array_salaries)
-    # print(int(avg_salaries))
-    #
-    #
-    # n = 60
-    #
-    # array_salaries = random.choices(test_generate_array, k=n)
-    # print()
-    # x = sorted(list(map(int, array_salaries)))
-    # print(x)
-    print(sorted(salaries))
-    print(len(salaries))
+    logger.info(f"len salaries -  {len(salaries)}")
+    logger.debug(f"len salaries - {len(salaries)}")
+    logger.info(f"max salaries -  {max(array_salaries)}")
+    logger.debug(f"min salaries - {min(array_salaries)}")
     salaries.remove(max(array_salaries)), salaries.remove(min(array_salaries))
-    print(sorted(salaries))
-    print(len(salaries))
-    print()
-
-    avg_salaries = sum(salaries) / len(salaries)
-    print(round(avg_salaries, 2))
+    logger.info("max/min salaries - deleted")
+    logger.debug("max/min salaries - deleted")
+    avg_salaries = round(sum(salaries) / len(salaries), 2)
+    logger.info(f"avg_salaries - {avg_salaries}")
+    logger.debug("End program!")
     return avg_salaries
-#
-# start= round(random.uniform(33, 66), 2)
-# end = round(random.uniform(33, 66), 2)
-# salaries_array = list(range(start, end))
-#
-# start = 20000
-# end = 30000
 
-# sampl = numpy.random.uniform(low=start, high=end, size=(50,))
-# y=list(map(lambda i: i.round(2), sampl))
 
-n = 60
+if __name__ == "__main__":
+    logger = logging.getLogger("check")
 
-start = 20000
-end = 30000
-# array_salaries = random.choices(test_generate_array, k=n)
-array_salaries = np.random.uniform(low=start, high=end, size=(n,))
-# print(sorted(array_salaries))
+    logging.basicConfig(
+        level=logging.DEBUG, filename='logs.log',
+        format='%(levelname)s --- %(name)s - %(asctime)s %(message)s',
+        filemode="w"
+    )
 
-get_average_salary_corrected(list(array_salaries))
+    n = 60
+    start = 20000
+    end = 30000
+    array_salaries = np.random.uniform(low=start, high=end, size=(n,))
+
+    logger.debug("Start program!")
+    logger.debug(f"O(N) = {n}")
+
+    print(get_average_salary_corrected(list(array_salaries)))
