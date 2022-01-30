4c6c7b4e1dce2fe04a5a62a78448f2ea8e76ff8e
vlaskinmac <kangelina86@mail.ru>
2022-01-30 11:53:08 +0300

    Done hw_10

--- module_06_debugging_begin/hw/hw_10_my_t9.py
+++ module_06_debugging_begin/hw/hw_10_my_t9.py
@@ -27,8 +27,42 @@
 Ваше решение должно работать с алгоритмической сложностью O(N),
 где N -- длина цифровой строки.
 """
-from typing import List
+
+from collections import Counter
+import re
+import itertools
 
 
-def my_t9(input_numbers: str) -> List[str]:
-    pass
+all_words=Counter()
+n2l={
+    2:'abc',
+    3:'def',
+    4:'ghi',
+    5:'jkl',
+    6:'mno',
+    7:'pqrs',
+    8:'tuv',
+    9:'wxyz'
+}
+
+
+with open('text.txt','r') as fin:
+    for line in fin:
+         all_words.update([word.lower() for word in re.findall(r'\b\w+\b',line)])
+
+
+def combos(*nums):
+    t=[n2l[i] for i in nums]
+    return tuple(''.join(t) for t in itertools.product(*(t)))
+
+
+def my_t9(*nums):
+    combo=combos(*nums)
+    c1=combos(nums[0])
+    first_cut=(word for word in all_words if word.startswith(c1))
+    res = (word for word in first_cut if word.startswith(combo))
+    print(list(res))
+
+
+if __name__ == "__main__":
+    my_t9(7, 2)
