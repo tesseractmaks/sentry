88cf739c99ab430c328f335e61e21d9627847d51
vlaskinmac <kangelina86@mail.ru>
2022-01-30 10:53:47 +0300

    Done hw_9

--- module_06_debugging_begin/hw/hw_9_straight_line.py
+++ module_06_debugging_begin/hw/hw_9_straight_line.py
@@ -12,4 +12,15 @@ from typing import List, Tuple
 
 
 def check_is_straight_line(coordinates: List[Tuple[float, float]]) -> bool:
-    pass
+    x_1, x_2, x_3, y_1, y_2, y_3 = list(*coordinates)
+    if ((x_3 - x_1) / (x_2 - x_1) == (y_3 - y_1) / (y_2 - y_1)):
+        print('лежат')
+    else:
+        print('не лежат')
+
+
+if __name__ == '__main__':
+    user_numbers = input()
+    coordinates = []
+    coordinates.append(map(float, list(user_numbers)))
+    check_is_straight_line(coordinates)
