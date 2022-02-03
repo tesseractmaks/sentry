759a5b40f41d14eda2003e2167398cfaaefdde13
vlaskinmac <kangelina86@mail.ru>
2022-02-03 10:26:03 +0300

    Done task 3

--- module_07_logging_part_2/homework/application/app.py
+++ module_07_logging_part_2/homework/application/app.py
@@ -1,9 +1,12 @@
+import logging.config
 import sys
 
+from mod_logger import dict_config
 from utils import string_to_operator
 
 
 def calc(args):
+    logger_debug.debug(f"Arguments: {args}")
     print("Arguments: ", args)
 
     num_1 = args[0]
@@ -13,22 +16,34 @@ def calc(args):
     try:
         num_1 = float(num_1)
     except ValueError as e:
-        print("Error while converting number 1")
-        print(e)
+        logger_error.error(f"Error while converting number 1 {e}")
 
     try:
         num_2 = float(num_2)
     except ValueError as e:
-        print("Error while converting number 1")
-        print(e)
+        logger_error.error(f"Error while converting number 1 {e}")
 
     operator_func = string_to_operator(operator)
 
     result = operator_func(num_1, num_2)
-
+    logger_debug.debug(f"Result:  {result}")
     print("Result: ", result)
     print(f"{num_1} {operator} {num_2} = {result}")
 
 
 if __name__ == '__main__':
+    logging.config.dictConfig(dict_config)
+
+    logger_debug = logging.getLogger("module_logger_debug")
+    logger_error = logging.getLogger("module_logger_error")
+    logger_debug.setLevel("DEBUG")
+    logger_error.setLevel("ERROR")
+
+
+
+    # logging.basicConfig(
+    #     level=logging.DEBUG, filename='logs.log',
+    #     format='%(levelname)s --- %(name)s - %(asctime)s %(message)s',
+    #     filemode="w"
+    # )
     calc(sys.argv[1:])
--- module_07_logging_part_2/homework/application/utils.py
+++ module_07_logging_part_2/homework/application/utils.py
@@ -1,12 +1,17 @@
+import logging
+
 from typing import Union, Callable
-from operator import sub, mul, div, add
+from operator import sub, mul, floordiv, add
+
+
+logger = logging.getLogger(__name__)
 
 
 OPERATORS = {
     '+': add,
     '-': sub,
     '*': mul,
-    '/': div
+    '/': floordiv
 }
 
 Numeric = Union[int, float]
@@ -18,11 +23,11 @@ def string_to_operator(value: str) -> Callable[[Numeric, Numeric], Numeric]:
     :param value: basic arithmetic function
     """
     if not isinstance(value, str):
-        print("wrong operator type", value)
+        logger.error(f"wrong operator type {value}")
         raise ValueError("wrong operator type")
 
     if value not in OPERATORS:
-        print("wrong operator value", value)
+        logger.error(f"wrong operator type {value}")
         raise ValueError("wrong operator value")
 
     return OPERATORS[value]
