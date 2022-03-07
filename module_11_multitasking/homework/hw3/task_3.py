2161d162469a70274038596fb416a54ef5fe1685
vlaskinmac <kangelina86@mail.ru>
2022-03-07 18:09:37 +0300

    Done task_3

--- module_11_multitasking/homework/hw3/task_3.py
+++ module_11_multitasking/homework/hw3/task_3.py
@@ -8,25 +8,25 @@ from requests import HTTPError
 from itertools import count
 
 
-def get_content():
-    url = 'https://swapi.dev/api/people/'
-    param = {}
-    for page in count(1):
-        param["page"] = page
-        response = requests.get(url, params=param)
-        response.raise_for_status()
-        people_content = response.json()['results']
-        for people in people_content:
-            try:
-                yield people
-            except IndexError:
-                break
-
-
 class StarWarsPeople(threading.Thread):
     def __init__(self, people, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.people = people
+
+    @staticmethod
+    def get_content():
+        url = 'https://swapi.dev/api/people/'
+        param = {}
+        for page in count(1):
+            param["page"] = page
+            response = requests.get(url, params=param)
+            response.raise_for_status()
+            people_content = response.json()['results']
+            for people in people_content:
+                try:
+                    yield people
+                except IndexError:
+                    break
 
     def run(self):
         people = self.people
@@ -44,7 +44,7 @@ if __name__ == '__main__':
     start = time.time()
     count_people = 0
     try:
-        for people in get_content():
+        for people in StarWarsPeople.get_content():
             object_thread = StarWarsPeople(people=people)
             object_thread.start()
             count_people += 1
