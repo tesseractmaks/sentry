1f82400f6d5222daa057f6dd91ea93a1251bcd33
Skillbox <it@skillbox.ru>
2022-03-14 06:44:40 +0000

    Add lesson

---
+++ module_12_multitasking_2/homework/hw2/task.py
@@ -0,0 +1,7 @@
+def process_count(username: str) -> int:
+    # к-во процессов, запущенных из-под текущего пользователя
+    pass
+
+def total_memory_usage(root_pid: int) -> int:
+    # суммарное потребление памяти древа процессов
+    pass
