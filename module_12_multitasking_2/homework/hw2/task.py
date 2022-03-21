113feacde284c2462ee0f26c9838c3ce84d5199f
vlaskinmac <tesseractmaks@gmail.com>
2022-03-21 18:33:51 +0300

    Done task2

--- module_12_multitasking_2/homework/hw2/task.py
+++
@@ -1,7 +0,0 @@
-def process_count(username: str) -> int:
-    # к-во процессов, запущенных из-под текущего пользователя
-    pass
-
-def total_memory_usage(root_pid: int) -> int:
-    # суммарное потребление памяти древа процессов
-    pass
