ba8d57b4b42047033eca1eb639d26e8717b42562
vlaskinmac <tesseractmaks@gmail.com>
2022-07-15 12:32:43 +0300

    Done task 5

--- module_13_db2/hw/05_hw_occult_car_repair.py
+++ module_13_db2/hw/05_hw_occult_car_repair.py
@@ -11,3 +11,32 @@
     и в ответ возвращает число клиентов, которые могли бы воспользоваться скидкой автосалона.
     Таблица с данными называется `table_occult_car_repair`
 """
+import sqlite3
+
+sql_request = """
+SELECT COUNT(*) FROM table_occult_car_repair WHERE (strftime('%m', timestamp)=? AND car_colour = 'чёрный') AND (car_type='Лада' OR car_type='BMW')
+"""
+
+
+def get_number_of_luckers(
+        c: sqlite3.Cursor, month_number: str,
+):
+    c.execute(sql_request, (month_number, ))
+    number_of_luckers, *_ = c.fetchall()
+    print("Число счастливых клиентов:", int(*number_of_luckers))
+
+
+if __name__ == "__main__":
+    month_number = input("Введите номер месяца: ")
+    while True:
+        if 1 <= int(month_number) <= 12:
+            break
+        print("Введите номер месяца в диапазоне 1-12")
+        month_number = input("Введите номер месяца: ")
+
+    with sqlite3.connect("hw.db") as conn:
+        cursor = conn.cursor()
+        if 1 <= int(month_number) < 10:
+            month_number = f"0{month_number}"
+        get_number_of_luckers(cursor, month_number)
+
