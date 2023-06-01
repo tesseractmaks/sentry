c2782bec894859dd3788cfc7676a562ddc5c1351
vlaskinmac <tesseractmaks@gmail.com>
2023-06-01 18:05:23 +0800

    Done task-1

---
+++ module_16_db3/homework/task_1_create_schema.py
@@ -0,0 +1,72 @@
+import sqlite3
+
+ENABLE_FOREIGN_KEY = "PRAGMA foreign_keys = ON;"
+
+CREATE_DIRECTOR_TABLE = """
+DROP TABLE IF EXISTS 'director';
+CREATE TABLE 'director' (
+    dir_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    dir_first_name VARCHAR(50) NOT NULL,
+    dir_last_name VARCHAR(50) NOT NULL
+) 
+"""
+
+CREATE_MOVIE_TABLE = """
+DROP TABLE IF EXISTS 'movie';
+CREATE TABLE 'movie' (
+    mov_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    mov_title VARCHAR(50) NOT NULL
+) 
+"""
+
+CREATE_ACTORS_TABLE = """
+DROP TABLE IF EXISTS 'actors';
+CREATE TABLE 'actors' (
+    act_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    act_first_name VARCHAR(50) NOT NULL,
+    act_last_name VARCHAR(50) NOT NULL,
+    act_gender VARCHAR(1) NOT NULL
+) 
+"""
+
+CREATE_MOVIE_DIRECTION_TABLE = """
+DROP TABLE IF EXISTS 'movie_direction';
+CREATE TABLE 'movie_direction' (
+    dir_id INTEGER NOT NULL REFERENCES director (dir_id) ON DELETE CASCADE,
+    mov_id INTEGER NOT NULL REFERENCES movie (mov_id) ON DELETE CASCADE
+) 
+"""
+
+CREATE_OSCAR_AWARDED_TABLE = """
+DROP TABLE IF EXISTS 'oscar_awarded';
+CREATE TABLE 'oscar_awarded' (
+    award_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    mov_id INTEGER NOT NULL REFERENCES movie (mov_id) ON DELETE CASCADE
+) 
+"""
+
+CREATE_MOVIE_CAST_TABLE = """
+DROP TABLE IF EXISTS 'movie_cast';
+CREATE TABLE 'movie_cast' (
+    act_id INTEGER NOT NULL REFERENCES actors (act_id) ON DELETE CASCADE,
+    mov_id INTEGER NOT NULL REFERENCES movie (mov_id) ON DELETE CASCADE,
+    role VARCHAR(50) NOT NULL
+) 
+"""
+
+
+def create_tables():
+    with sqlite3.connect("surrogate.db") as conn:
+        cursor = conn.cursor()
+
+        cursor.executescript(ENABLE_FOREIGN_KEY)
+        cursor.executescript(CREATE_DIRECTOR_TABLE)
+        cursor.executescript(CREATE_MOVIE_TABLE)
+        cursor.executescript(CREATE_ACTORS_TABLE)
+        cursor.executescript(CREATE_MOVIE_DIRECTION_TABLE)
+        cursor.executescript(CREATE_OSCAR_AWARDED_TABLE)
+        cursor.executescript(CREATE_MOVIE_CAST_TABLE)
+
+
+if __name__ == '__main__':
+    create_tables()
